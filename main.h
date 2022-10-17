b3c3802c171746f5b91549636a423199195b8df5
Jerry Nwadinobi <fernandojerry19911500@gmail.com>
2022-10-17 09:27:24 -0700

    printf project

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _PRINT_F
+#define _PRINT_F
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+#endif
